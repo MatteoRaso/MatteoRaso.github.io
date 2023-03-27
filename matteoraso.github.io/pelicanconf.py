dcbb2fdfbfe7c339ec86297b4faeffccee68381f
Matteo Raso <matteo_luigi_raso@protonmail.com>
2023-03-27 01:02:57 -0400

    Changed website theme

--- matteoraso.github.io/pelicanconf.py
+++ matteoraso.github.io/pelicanconf.py
@@ -8,6 +8,10 @@ TIMEZONE = 'America/Toronto'
 
 DEFAULT_LANG = 'en'
 
+THEME = "/home/matteo/pelican-themes/taman/"
+
+READERS = {'html': None}
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -16,14 +20,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Github', 'https://github.com/MatteoRaso'),)
+#         ('Python.org', 'https://www.python.org/'),
+#         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
